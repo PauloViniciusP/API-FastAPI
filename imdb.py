aa956d4ea15685bfe7fc31bd9011fdc53c435e58
Paulo Nascimento <nexz265@gmail.com>
2024-06-07 19:54:13 -0300

    Updated to read json files

--- imdb.py
+++ imdb.py
@@ -1,10 +1,8 @@
 from product import Product
+import json
 
 def generate_products():
-    list_products = []
-
-    for x in range(10):
-        p = Product(name = f"Product {x+1}", price = 6.9 * (x+1))
-        list_products.append(p)
-    
-    return list_products
+    f = open('./data/products.json')
+    data = json.loads(f.read())
+    f.close()
+    return data
