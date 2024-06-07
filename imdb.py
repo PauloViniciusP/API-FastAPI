8f172a928653542d1eebefecfb5556c46c3c59f0
Paulo Nascimento <nexz265@gmail.com>
2024-06-07 18:56:16 -0300

    Updated main and imdb

--- imdb.py
+++ imdb.py
@@ -2,3 +2,9 @@ from product import Product
 
 def generate_products():
     list_products = []
+
+    for x in range(10):
+        p = Product(name = f"Product {x+1}", price = 6.9 * (x+1))
+        list_products.append(p)
+    
+    return list_products
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from imdb import generate_products
 
 app = FastAPI()
 
+products = generate_products()
+
 @app.get('/')
-def get_languages():
-    return{'success': 'working'}
+def get_products():
+    return{'Products': products}
