6b26e05a067df59428933adbe9f4b79d07444095
Paulo Nascimento <nexz265@gmail.com>
2024-06-07 23:00:25 -0300

    Updated methods

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
+from json_db import JsonDB
 from imdb import generate_products
 from product import Product
 
 app = FastAPI()
 
-products = generate_products()
+productDB = JsonDB(path='.data/products.json')
 
 @app.get('/products')
 def get_products():
+    products = productDB.read()
     return{'Products': products}
 
 @app.post('/products')
